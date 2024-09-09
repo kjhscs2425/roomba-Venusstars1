c197b756cdff6afab84442a8778f508ae7310e7c
Venusstars1 <nkohavi2028@kehillahstudent.org>
2024-09-09 09:23:16 -0700

    Done with turtle

--- roomba_level0.py
+++ roomba_level0.py
@@ -4,7 +4,7 @@
 #
 # Author: Dr. EB <------ REPLACE THIS WITH YOUR NAME!
 # -----------------------------------------------------------------------------
- 
+import turtle 
 from turtle import right, left, forward, backward
 import room
 
@@ -18,5 +18,17 @@ window = room.draw_room(level = 0)
  
 # End your code here
 ###
- 
+for i in range(3):
+    turtle.forward (160)
+    turtle.left (90)
+for i in range (2):
+    turtle.forward (120)
+    turtle.left(90)
+for i in range (2):
+    turtle.forward (80)
+    turtle.left(90)
+for i in range (2):
+    turtle.forward(40)
+    turtle.left(90)
+
 window.exitonclick()
--- roomba_level3.py
+++ roomba_level3.py
@@ -9,14 +9,70 @@ from turtle import right, left, forward, backward, speed
 import room
 
 # Make the turtle go faster
-speed(7)
+speed(4)
 
 # Draw the Level 3 version of the room
-window = room.draw_room(level = 3)
+window = room.draw_room(level = 3, radius=5)
 
 ###
 # Start your code here
- 
+import turtle
+turtle.forward (40)
+turtle.right(90)
+turtle.forward (120)
+turtle.left (90)
+turtle.forward (40)
+turtle.right (90)
+turtle.forward (40)
+turtle.left(90)
+turtle.forward (240)
+
+turtle.left (90)
+turtle.forward (40)
+turtle.right (90)
+turtle.forward (40)
+turtle.left(90)
+turtle.forward (240)
+
+turtle.left (90)
+turtle.forward (40)
+turtle.right (90)
+turtle.forward (40)
+turtle.left (90)
+turtle.forward (240)
+
+turtle.left (90)
+turtle.forward (40)
+turtle.right (90)
+turtle.forward (40)
+
+turtle.left (180)
+turtle.forward (280)
+for i in range (3):
+    turtle.right (90)
+    turtle.forward (40)
+    turtle.right (90)
+    turtle.forward (280)
+    turtle.left (90)
+    turtle.forward (40)
+    turtle.left (90)
+    turtle.forward (280)
+
+turtle.left (180)
+turtle.forward (120)
+turtle.left (90)
+turtle.forward (80)
+turtle.left (180)
+turtle.forward (200)
+turtle.right(90)
+turtle.forward (200)
+turtle.left (180)
+turtle.forward (200)
+turtle.right (90)
+turtle.forward (200)
+
+   
+
  
  
 # End your code here
